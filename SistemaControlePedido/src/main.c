9c0a8e4f58855b7f8c0c578cef6c9bb62c885e67
antonyasfr <antonysilva03022007@gmail.com>
2025-11-09 15:30:48 -0300

    Atualização do módulo Produto com menu em Ncurses

--- SistemaControlePedido/src/main.c
+++ SistemaControlePedido/src/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 #include "../include/produto.h"
 
 int main() {
     int opcao;
 
-    do {
-        printf("\n -> MENU DE PRODUTOS <- \n");
-        printf("1. Cadastrar produto\n");
-        printf("2. Listar produtos\n");
-        printf("3. Atualizar produto\n");
-        printf("4. Deletar produto\n");
-        printf("0. Sair\n");
-        printf("--------------------\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
+    initscr();
+    echo();
+    cbreak();
+    curs_set(1);
 
+    do {
+        clear();
+
+        mvprintw(2, 10, " -> MENU DE PRODUTOS <- ");
+        mvprintw(5, 12, "1. Cadastrar produto\n");
+        mvprintw(6, 12, "2. Listar produtos\n");
+        mvprintw(7, 12, "3. Atualizar produto\n");
+        mvprintw(8, 12, "4. Deletar produto\n");
+        mvprintw(9, 12, "0. Sair\n");
+        mvprintw(11, 10, "--------------------\n");
+        mvprintw(13, 10, "Escolha uma opcao: ");
+        refresh();
+
+        scanw("%d", &opcao);
+        
+        clear();
         switch (opcao) {
             case 1:
                 cadastrarProduto();
@@ -30,13 +41,21 @@ int main() {
                 deletarProduto();
                 break;
             case 0:
-                printf("\nSaindo do sistema...\n");
+                mvprintw(5, 10, "\nSaindo do sistema...\n");
+                refresh();
                 break;
             default:
-                printf("\nOpcao invalida! Tente novamente.\n");
+                mvprintw(5, 10, "\nOpcao invalida! Tente novamente.\n");
+                refresh();
+        }
+        if (opcao != 0) {
+        mvprintw(20, 10, "Pressione qualquer tecla para continuar!\n");
+        refresh();
+        getch();
         }
 
     } while (opcao != 0);
-
+    
+    endwin();
     return 0;
 }
--- SistemaControlePedido/src/produto.c
+++ SistemaControlePedido/src/produto.c
@@ -1,52 +1,66 @@
 #include "../include/produto.h"
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ARQUIVO_PRODUTOS "../data/Produtos.csv"
+#define ARQUIVO_PRODUTOS "data/Produtos.csv"
 
 void cadastrarProduto() {
     Produto p;
     FILE *arquivo = fopen(ARQUIVO_PRODUTOS, "a");
 
     if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo de produtos!\n");
+        mvprintw(5, 10, "Erro ao abrir o arquivo de produtos!\n");
+        refresh();
+        getch();
         return;
     }
-
-    printf("Digite o ID do produto:");
-    scanf("%d", &p.id);
+    echo();
+    
+    mvprintw(4, 10, "Digite o ID do produto:");
+    scanw("%d", &p.id);
     getchar();
-    printf("Digite o nome do produto:");
 
-    fgets(p.nome, 50, stdin);
-    p.nome[strcspn(p.nome, "\n")] = '\0';
+    mvprintw(5, 10, "Digite o nome do produto:");
+    getnstr(p.nome, 50);
 
-    printf("Digite o preco do produto:");
-    scanf("%f", &p.preco);
-    printf("Digite a quantidade do produto:");
-    scanf("%d", &p.quantidade);
+
+    mvprintw(6, 10, "Digite o preco do produto:");
+    scanw("%f", &p.preco);
+
+    mvprintw(7, 10, "Digite a quantidade do produto:");
+    scanw("%d", &p.quantidade);
 
     fprintf(arquivo, "%d,%s,%.2f,%d\n", p.id, p.nome, p.preco, p.quantidade);
     fclose(arquivo);
-    printf("Produto cadastrado com sucesso!\n");
+    mvprintw(9, 10, "Produto cadastrado com sucesso!\n");
+    refresh();
+    getch();
 }
     
     void listarProdutos() {
         Produto p;
         FILE *arquivo = fopen(ARQUIVO_PRODUTOS, "r");
 
+        
+
         if (arquivo == NULL) {
-            printf("Nenhum produto cadastrado ainda.\n");
+            mvprintw(4, 10, "Nenhum produto cadastrado ainda.\n");
+            refresh();
+            getch();
             return;
         }
-
-        printf("Lista de Produtos:\n");
+        
+        int linha = 5;
         while (fscanf(arquivo, "%d,%49[^,],%f,%d\n", &p.id, p.nome, &p.preco, &p.quantidade) != EOF) {
-            printf("ID: %d, Nome: %s, Preco: %.2f, Quantidade: %d\n", p.id, p.nome, p.preco, p.quantidade);
+            mvprintw(linha++, 10, "ID: %d, Nome: %s, Preco: %.2f, Quantidade: %d\n", p.id, p.nome, p.preco, p.quantidade);
         }
 
         fclose(arquivo);
+        mvprintw(linha + 2, 10, "Fim da lista de produtos.\n");
+        refresh();
+        getch();
     }
 
     void atualizarProduto() {
@@ -57,24 +71,26 @@ void cadastrarProduto() {
         FILE *temp = fopen("data/temp.csv", "w");
 
         if (arquivo == NULL || temp == NULL) {
-            printf("Erro ao abrir o arquivo de produtos!\n");
+            mvprintw(4, 10, "Erro ao abrir o arquivo de produtos!\n");
+            refresh();
+            getch();
             return;
         }
 
-        printf("Digite o ID do produto que deseja atualizar: ");
-        scanf("%d", &idBusca);
+        mvprintw(4, 10, "Digite o ID do produto que deseja atualizar: ");
+        scanw("%d", &idBusca);
 
         while (fscanf(arquivo, "%d,%49[^,],%f,%d\n", &p.id, p.nome, &p.preco, &p.quantidade) != EOF) {
             if (p.id == idBusca) {
-                printf("Digite o novo nome do produto: ");
-                getchar();
-                fgets(p.nome, 50, stdin);
-                p.nome[strcspn(p.nome, "\n")] = '\0';
+                mvprintw(6, 10, "Digite o novo nome do produto: ");
+                echo();
+                getnstr(p.nome, 50);
+                noecho();
 
-                printf("Digite o novo preco do produto: ");
-                scanf("%f", &p.preco);
-                printf("Digite a nova quantidade do produto: ");
-                scanf("%d", &p.quantidade);
+                mvprintw(7, 10, "Digite o novo preco do produto: ");
+                scanw("%f", &p.preco);
+                mvprintw(8, 10, "Digite a nova quantidade do produto: ");
+                scanw("%d", &p.quantidade);
 
                 encontrado = 1;
             }
@@ -87,9 +103,12 @@ void cadastrarProduto() {
         rename("data/temp.csv", ARQUIVO_PRODUTOS);
 
         if (encontrado)
-            printf("Produto atualizado com sucesso!\n");
+            mvprintw(10, 10, "\nProduto atualizado com sucesso!\n");
             else
-            printf("Produto com ID %d nao encontrado.\n", idBusca);
+            mvprintw(10, 10, "Produto com ID %d nao encontrado.\n", idBusca);
+
+            refresh();
+            getch();
     }
 
     void deletarProduto() {
@@ -100,13 +119,16 @@ void cadastrarProduto() {
         FILE *arquivo = fopen(ARQUIVO_PRODUTOS, "r");
         FILE *temp = fopen("data/temp.csv", "w");
 
+
         if (arquivo == NULL || temp == NULL) {
-            printf("Erro ao abrir o arquivo de produtos!\n");
+            mvprintw(4, 10, "Erro ao abrir o arquivo de produtos!\n");
+            refresh();
+            getch();
             return;
         }
 
-        printf("Digite o ID do produto que deseja deletar: ");
-        scanf("%d", &idBusca);
+        mvprintw(4, 10, "Digite o ID do produto que deseja deletar: ");
+        scanw("%d", &idBusca);
 
         while (fscanf(arquivo, "%d,%49[^,],%f,%d\n", &p.id, p.nome, &p.preco, &p.quantidade) != EOF) {
             if (p.id == idBusca) {
@@ -121,8 +143,11 @@ void cadastrarProduto() {
         remove(ARQUIVO_PRODUTOS);
         rename("data/temp.csv", ARQUIVO_PRODUTOS);
 
-        if (encontrado)
-            printf("Produto deletado com sucesso!\n");
-        else
-            printf("Produto com ID %d nao encontrado.\n", idBusca);
+        if (encontrado){
+            mvprintw(6, 10, "\nProduto deletado com sucesso!\n");
+        } else {
+            mvprintw(6, 10, "Produto com ID %d nao encontrado.\n", idBusca);
+        }
+            refresh();
+            getch();
     }
